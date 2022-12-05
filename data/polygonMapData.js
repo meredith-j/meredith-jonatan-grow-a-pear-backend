911bf49b5eb13bf55010cce35e9accffb27a579d
Meredith Jonatan <mjonatan@me.com>
2022-12-05 16:49:18 -0500

    add initial databases

--- data/polygonMapData.js
+++ data/polygonMapData.js
@@ -1,22 +1,45 @@
 const classifyPoint = require("robust-point-in-polygon");
 
+// NEXT TWO LINES ARE THE EXAMPLE THE CLASSIFY POINT NPM LIBRARY PROVIDED
+// const test = [[-74, 50], [-80, 47], [-74, 45], [-70, 47]]
+// console.log(classifyPoint(test, [-73.5698065, 45.5031824]));
+
+// HERE IS THE ARRAY OF EVERY POLYGON I HAVE PLACED ON MY MAP
 const zones = [
-[[-104, 47], [-100, 47], [-100, 50], [-104, 45] ],
-[[-64, 47], [-70, 47], [-80, 50], [-74, 45] ]
-]
+// zone 1
+[[-104, 47], [-120, 47], [-100, 50], [-144, 45]],
+// zone 2
+[[-114, 20], [-100, 47], [-100, 50], [-104, 45]],
+[[-104, 47], [-100, 47], [-100, 50], [-104, 45]],
+[[-74, 50], [-80, 47], [-74, 45], [-70, 47]]
+];
 
-function pleaseWork(long, lat) {
-    for (i = 0; i < zones.length; i++) {
+// THIS FUNCTION LOOPS THROUGH ALL OF THE POLYGONS ON MY MAP AND USES THE CLASSIFYPOINT FUNCTION TO DETERMINE WHETHER EACH ZONE SURROUNDS THE COORDINATES (LONG/LAT)
+function findZone(long, lat) {
+    for (let i = 0; i < zones.length; i++) {
     
-    result = classifyPoint(zones, [long, lat])
+    // in case i need to test again:
+    // console.log('zone', zones[i])
+    
+    let result = classifyPoint(zones[i], [long, lat])
 
-        if (result = -1 && (i = 0)) {
+    // in case i need to test again
+    // console.log(result)
+
+        if (result === -1 && (i < 1)) {
             return "try again :("
         }
-        if (result = -1 && (i = 1)) {
+        if (result === -1 && (i < 2)) {
+            return "nope"
+        }
+        if (result === -1 && (i < 3)) {
+            return "close"
+        }
+        if (result === -1 && (i < 4)) {
             return "YAHOO"
         }
     }
 }
 
-console.log(pleaseWork(-73.5698065, 45.5031824))
+// TESTING PURPOSES
+console.log(findZone(-73.5698065, 45.5031824))
---
+++ seed_data/plantDetailsData.js
@@ -0,0 +1,70 @@
+module.exports = [
+    {
+    id: 1,
+    name: 'Killarney Raspberry',
+    care_instructions: 'MORE TBD',
+    info: 'The Killarney raspberry is a hearty fruit that can survive in extremely cold climates - in many cases, it will even live through your frosty winter! Raspberries need time to get used to their new home, so it will likely take a season before you can harvest fruit',
+    category: 'Fruit',
+    needs_trellis: false,
+    image: url('./images/raspberry-killarney.jpg'),
+    created_on: 'December 1, 2022',
+    updated_on: 'December 1, 2022'
+  },
+  {
+    id: 2,
+    name: 'Encore Raspberry',
+    care_instructions: 'MORE TBD',
+    info: 'These berries are known to be super sweet and are often used to make pie filling & homemade jams. Raspberries need time to get used to their new home, so it will likely take a season before you can harvest fruit.',
+    category: 'Fruit',
+    needs_trellis: false,
+    image: url('./images/raspberry-encore.jpg'),
+    created_on: 'December 1, 2022',
+    updated_on: 'December 1, 2022'
+  },
+  {
+    id: 3,
+    name: 'Wild Strawberry',
+    care_instructions: 'MORE TBD',
+    info: "Wild strawberries are seedless and slightly smaller than the typical strawberries you'll find at a grocery store - but are still sweet & flavourful. Strawberries need time to get used to their new home, so it will likely take a season before you can harvest fruit.",
+    category: 'Fruit',
+    needs_trellis: false,
+    image: url('./images/strawberry-wild.jpeg'),
+    created_on: 'December 1, 2022',
+    updated_on: 'December 1, 2022'
+  },
+  {
+    id: 4,
+    name: 'Kent Strawberry',
+    care_instructions: 'MORE TBD',
+    info: 'Kent strawberries are a medium sized strawberry that is packed with flavour - enjoy with some jam or on its own! Strawberries need time to get used to their new home, so it will likely take a season before you can harvest fruit.',
+    category: 'Fruit',
+    needs_trellis: false,
+    image: url('./images/strawberry-kent.jpeg'),
+    created_on: 'December 1, 2022',
+    updated_on: 'December 1, 2022'
+  },
+  {
+    id: 5,
+    name: 'Prairie Sensation Apple',
+    care_instructions: 'MORE TBD',
+    info: '',
+    category: 'Fruit',
+    needs_trellis: false,
+    image: url('./images/tree-prairie-sensation-apple.jpeg'),
+    created_on: 'December 1, 2022',
+    updated_on: 'December 1, 2022'
+  }
+]
+
+
+// {
+//   id: ,
+//   name: '',
+//   care_instructions: '',
+//   info: '',
+//   category: '',
+//   needs_trellis: ,
+//   image: url(''),
+//   created_on: 'December 1, 2022',
+//   updated_on: 'December 1, 2022'
+// },
