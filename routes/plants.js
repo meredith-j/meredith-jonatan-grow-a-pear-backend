ed5a0cb403c626ca46690174872f6ccb1419d01e
Meredith Jonatan <mjonatan@me.com>
2022-12-08 16:03:22 -0500

    saving before debugging

---
+++ routes/plants.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors")
+// const plantsController = require("./controllers/plantsController.js");
+
+// router
+//     .route("/")
+//     .get(plantsController.getAll)
+
+//     module.exports = router;
