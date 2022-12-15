62ae39c13a4b9b4be44061aede70563c2cc37920
Meredith Jonatan <mjonatan@me.com>
2022-12-15 03:02:24 -0500

    fix API key issues

--- seed_data/plantDetailsData.js
+++ seed_data/plantDetailsData.js
@@ -6,7 +6,7 @@ module.exports = [
     info: 'The Killarney raspberry is a hearty fruit that can survive in extremely cold climates - in many cases, it will even live through your frosty winter! Raspberries need time to get used to their new home, so it will likely take a season before you can harvest fruit',
     category: 'Fruit',
     needs_trellis: false,
-    image: './images/raspberry-killarney.jpg',
+    image: 'http://localhost:8080/images/raspberry-killarney.jpg',
   },
   {
     id: 2,
@@ -15,7 +15,7 @@ module.exports = [
     info: 'These berries are known to be super sweet and are often used to make pie filling & homemade jams. Raspberries need time to get used to their new home, so it will likely take a season before you can harvest fruit.',
     category: 'Fruit',
     needs_trellis: false,
-    image: './images/raspberry-encore.jpg'
+    image: 'http://localhost:8080/images/raspberry-encore.jpg'
   },
   {
     id: 3,
@@ -24,7 +24,7 @@ module.exports = [
     info: "Wild strawberries are seedless and slightly smaller than the typical strawberries you'll find at a grocery store - but are still sweet & flavourful. Strawberries need time to get used to their new home, so it will likely take a season before you can harvest fruit.",
     category: 'Fruit',
     needs_trellis: false,
-    image: './images/strawberry-wild.jpeg'
+    image: 'http://localhost:8080/images/strawberry-wild.jpeg'
   },
   {
     id: 4,
@@ -33,7 +33,7 @@ module.exports = [
     info: 'Kent strawberries are a medium sized strawberry that is packed with flavour - enjoy with some jam or on its own! Strawberries need time to get used to their new home, so it will likely take a season before you can harvest fruit.',
     category: 'Fruit',
     needs_trellis: false,
-    image: './images/strawberry-kent.jpeg'
+    image: 'http://localhost:8080/images/strawberry-kent.jpeg'
   },
   {
     id: 5,
@@ -42,7 +42,7 @@ module.exports = [
     info: '',
     category: 'Fruit',
     needs_trellis: false,
-    image: './images/tree-prairie-sensation-apple.jpeg'
+    image: 'http://localhost:8080/images/tree-prairie-sensation-apple.jpeg'
   }
 ]
 
