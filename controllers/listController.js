00156a0556dd7ef3cb52c64057a2b2daba41f221
Meredith Jonatan <mjonatan@me.com>
2023-01-12 10:49:23 -0500

    updating routse

--- controllers/listController.js
+++ controllers/listController.js
@@ -10,17 +10,39 @@ const knex = require("knex")(require("../knexfile"));
 //   };
 
 const postList = (req, res) => {
-    knex("user_list")
-    .join("plant_list", "plant_list.list_id", "user_list.id")
-    .where({ plant_id: req.params.id })
-    .where({ name: req.params.name})
-    .then((response) => {
+//     knex("user_list")
+//     .join("plant_list", "plant_list.list_id", "user_list.id")
+//     .where({ plant_id: req.params.id })
+//     .where({ name: req.params.name})
+//     .then((response) => {
+//         res.status(200).json(response)
+//     })
+//     .catch((err) => {
+//         res.status(err).json(response)
+//     })
+// }
+          
+    const { name } = req.body.name;
+      knex('user_list')
+      .insert({name})
+      .returning('id')
+      .then(([listId]) => {
+        // Use the generated list ID to insert the items into the "items" table
+        const plants = req.body.plants.map(plant => ({
+          ...plant,
+          plant_id: plant.id,
+          list_id: listId
+           }));
+        return knex('items').insert(items);
+        })
+      .then((response) => {
         res.status(200).json(response)
-    })
-    .catch((err) => {
-        res.status(err).json(response)
-    })
-}
+        })
+      .catch(err => {
+        console.error(err);
+        res.sendStatus(500);
+        });
+  };
 
 module.exports = {
     // getList,
--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const app = express();
 const fs = require("fs");
 const cors = require("cors");
 const plantsRoute = require("./routes/plants.js");
+const listRoute = require("./routes/plantList.js");
 
 const PORT = process.env.PORT || 8080;
 
@@ -13,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/plant", plantsRoute);
+app.use("/list", listRoute);
 
 
 app.listen(PORT);
